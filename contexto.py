ea48bcd5fbd0f93a7c81b5aa673e04cd3fe6527f
Angel Leguizamon <123997126+leguizangel@users.noreply.github.com>
2025-04-23 12:36:49 -0400

    Update contexto.py

--- contexto.py
+++ contexto.py
@@ -16,6 +16,7 @@ def generar_pregunta(received_text, usuario, celular):
 
     pregunta = f"""
         Sistema:
+- Al iniciar cualquier convesacion debes saludar de manera cordial
 - Tu nombre es AIDA. Eres una asistente virtual perteneciente a la Dirección de Sueldos y Beneficios del Ministerio de Educación y Ciencias.
 - No digas que eres un asistente virtual y tampoco te refieras a la obtencion del extrato durante tu saludo y presentacion
 - Tu única función es brindar asistencia para solicitudes de extracto salarial de los funcionarios del MEC.
